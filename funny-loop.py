5e159395fbc063276d2e259e45319f39055f88e9
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-02-07 14:26:40 +0100

    funny

---
+++ funny-loop.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+from os.path import expanduser
+import json
+import time
+install_directory = "Documents/GitHub/bad-apple-wallpaper/"
+user = "temmie"
+
+for aaaa in range(0,6000):
+	counter = 1
+	
+
+	with open(f"{install_directory}apple-counter.json", "r") as file:
+		count = json.load(file)
+
+	counter = count["counter"]
+
+	os.system(f"gsettings set org.gnome.desktop.background picture-uri file:///home/{user}/{install_directory}bad-apple/frame{counter}.png")
+
+	saving_counter = {}
+	saving_counter["counter"] = counter+1
+	with open(f"{install_directory}apple-counter.json", "w") as file:
+		json.dump(saving_counter, file)
+	time.sleep(0.02)
---
+++ wallpaperchanger.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+from os.path import expanduser
+import json
+
+install_directory = "Documents/GitHub/bad-apple-wallpaper/"
+user = "temmie"
+
+counter = 1
+
+with open(f"{install_directory}apple-counter.json", "r") as file:
+  count = json.load(file)
+
+counter = count["counter"]
+
+os.system(f"gsettings set org.gnome.desktop.background picture-uri file:///home/{user}/{install_directory}bad-apple/frame{counter}.png")
+
+saving_counter = {}
+saving_counter["counter"] = counter+1
+with open(f"{install_directory}apple-counter.json", "w") as file:
+  json.dump(saving_counter, file)
