a4ea74192df2a6167e3987611c74f89c5bd7a788
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-11-12 10:08:52 +0100

    cool wallpaper

--- wallpaperchanger.py
+++ wallpaperchanger.py
@@ -15,6 +15,6 @@ counter = count["counter"]
 os.system(f"gsettings set org.gnome.desktop.background picture-uri file:///home/{user}/{install_directory}bad-apple/frame{counter}.png")
 
 saving_counter = {}
-saving_counter["counter"] = counter+1
+saving_counter["counter"] = counter+4
 with open(f"{install_directory}apple-counter.json", "w") as file:
   json.dump(saving_counter, file)
